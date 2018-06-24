79be22bc6990cac34cf3fa93f77b6027d0f97c03
cloi1994 <31608472+cloi1994@users.noreply.github.com>
2018-06-24 14:04:54 -0700

    Update 252.py

--- 252.py
+++ 252.py
@@ -21,13 +21,13 @@ class Solution:
 
         intervals.sort(cmp=cmpfuc)
         
-        res = [intervals[0]]
-        
-        
-    
+
         for i in range(1,len(intervals)):
-            if intervals[i-1].end > intervals[i].start:
+            if intervals[i].start < intervals[i-1].end:
                 return False
                 
         return True
-
+            
+            
+            
+            
