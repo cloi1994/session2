19dd377fed8139869ecdd92b2021a36137bcc96f
cloi1994 <31608472+cloi1994@users.noreply.github.com>
2018-07-04 19:49:19 -0700

    Create 178.py

---
+++ 178.py
@@ -0,0 +1,49 @@
+class Solution:
+    """
+    @param n: An integer
+    @param edges: a list of undirected edges
+    @return: true if it's a valid tree, or false
+    """
+    def validTree(self, n, edges):
+        # write your code here
+        
+        graph = []
+        
+        for i in range(n):
+            graph.append([])
+            
+        for e in edges:
+            graph[e[0]].append(e[1])
+            graph[e[1]].append(e[0])
+           
+        visited = {0:1}
+        res = self.dfs(graph,visited,0,-1)
+        
+        print visited
+        if res == False:
+            return False
+            
+        if len(visited) != n:
+            return False
+            
+        return res
+    
+    def dfs(self,graph,visited,node,parent):
+        
+        children = graph[node]
+        
+        for c in children:
+            if c == parent:
+                continue
+            
+            if c in visited:
+                return False
+            
+            visited[c] = 1
+            if not self.dfs(graph,visited,c,node):
+                return False
+            
+        return True
+            
+            
+        
