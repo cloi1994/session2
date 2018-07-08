5c2cb333593ccf01cdf08480969efffe92123d5c
cloi1994 <31608472+cloi1994@users.noreply.github.com>
2018-07-07 18:34:01 -0700

    Create 98.py

---
+++ Amazon/98.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        
+        return self.dfs(root,float('-inf'),float('inf'))
+        
+        
+    def dfs(self,root,minLeft,maxRight):
+        
+        if not root:
+            return True
+        
+        if root.val <= minLeft or root.val >= maxRight:
+            return False
+        
+        return self.dfs(root.left,minLeft,root.val) and self.dfs(root.right,root.val,maxRight)
+        
