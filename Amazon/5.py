ebc26ce362e5bb5f14c8097e16a863d0497e91a9
cloi1994 <31608472+cloi1994@users.noreply.github.com>
2018-07-07 18:52:28 -0700

    Create 5.py

---
+++ Amazon/5.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+        self.left = 0
+        self.maxLen = 0
+        
+        for i in range(len(s)):
+            self.expand(i,i,s)
+            self.expand(i,i+1,s)
+            
+        return s[self.left:self.maxLen]
+            
+    def expand(self,left,right,s):
+        
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        
+        left += 1  #above exceeded
+        right -= 1
+        
+        
+        if self.maxLen < right - left + 1:
+            self.maxLen = right - left + 1
+            self.left = left
+        
+        
